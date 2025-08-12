56946eceefb3c9014925d252e739d474a5ca5020
Akshay Gupta <akshay@Akshays-MacBook-Pro-2.local>
2025-08-12 15:20:02 +0530

    add shelex

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  reporter: 'mocha', // optional, but must be a string
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
     },
-    baseUrl: "http://20.51.241.126:8080",
   },
-  reporter: "allure-cypress",
-  reporterOptions: {
-    resultsDir: "allure-results"
-  }
 });
