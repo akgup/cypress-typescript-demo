aa4ee5db4deb6919e6714a2c0dd3903930cab300
Akshay Gupta <akshay@Akshays-MacBook-Pro-2.local>
2025-08-12 15:24:18 +0530

    fix issue

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,21 @@ const { defineConfig } = require('cypress');
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  reporter: 'mocha', // optional, but must be a string
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
     },
+    baseUrl: 'http://localhost:3000', // change if needed
+    reporterOptions: {
+      reportDir: 'cypress/reports/mochawesome',
+      overwrite: false,
+      html: true,
+      json: true
+    }
   },
+  env: {
+    allure: true,
+    allureResultsPath: 'cypress/reports/allure-results'
+  }
 });
