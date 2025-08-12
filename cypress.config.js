3739e423a5510b8a3153c8bc8f0ac84ad8928b9e
Akshay Gupta <akshay@Akshays-MacBook-Pro-2.local>
2025-08-12 14:59:03 +0530

    add html publisher

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/html',
+      overwrite: true,
+      html: true,
+      json: false
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
